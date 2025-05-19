3d367b8ef0b4f04e88d4c2ec0c409b054ccf9e2b
otaviofg.alves <117609558+otaviofigueredoalves@users.noreply.github.com>
2025-05-19 19:56:21 -0300

    feat: mod2

---
+++ mod2/aula01/aula1-tarefa1.js
@@ -0,0 +1,3 @@
+let nome = "Otavio Figueredo Alves";
+let idade = 18;
+console.log("Meu nome é" + " " + nome + " " + "e" +  " " + "tenho" + " " + idade + " " + "anos");
---
+++ mod2/aula01/aula1-tarefa2.js
@@ -0,0 +1,4 @@
+let texto = "Eu nasci há tantos anos: ";
+let ano = 10;
+let frase = texto + ano;
+console.log(frase);
